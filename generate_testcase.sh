f51f2f3eaf0cc83d17db85c6714fbd95eafe16ef
Qiu Yuqing <qyuqing@umich.edu>
2023-04-27 20:22:13 -0400

    add testcase generator, makefile, rm algorithm

--- generate_testcase.sh
+++ generate_testcase.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-node=(40 80)
-edge=(100 210)
+node=(40)
+edge=(100)
 scale=(100)
 edgeTimeOut=3
 solveTimeOut=15
 #trial=10
 shortest=4
 longest=15
-target=2
+target=5
+dir="src/testcases/"
 for idx in ${!node[@]}
 do
   n=${node[$idx]}
@@ -17,17 +18,22 @@ do
   current=0
   while [ $current -lt $target ]
   do
-    output=$(./FourColor ${n} ${e} ${s} ${edgeTimeOut} ${solveTimeOut} ${inputFileName})
+    output=$(./fourcolor-release ${n} ${e} ${s} ${edgeTimeOut} ${solveTimeOut} ${inputFileName})
     retVal=$?
     timeCost=$(echo $output | tr -cd "[0-9]")
     echo "n is $n e is $e timeCost is $timeCost retVal is $retVal"
     if [ $retVal -eq 0 ]; then
-      if [ $timeCost -ge $shortest ] && [ $timeCost -le $longest ]; then
-        newFile="good_${n}_${e}_${timeCost}s.txt"
+      if [ $timeCost -ge $shortest ] && [ $timeCost -le $longest ]
+      then
+        newFile="${dir}/good_${n}_${e}_${timeCost}s.txt"
         inputFile="${inputFileName}.txt"
         cp $inputFile $newFile
         ((current=current+1))
       fi
     fi
   done
+  inputFile="${inputFileName}.txt"
+  outputFile="${inputFileName}_output.txt"
+  rm $inputFile
+  rm $outputFile
 done
