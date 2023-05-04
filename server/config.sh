2bf191854242275d67ea1c247b5db7bed970c381
Alexanderia-Mike <louchenfei8@gmail.com>
2023-05-04 16:30:56 -0400

    update testcase

--- server/config.sh
+++ server/config.sh
@@ -1,4 +1,5 @@
-./conversion testcases_conversion/20x20_3_2_inputs.txt false
-./conversion testcases_conversion/200x200_369_445_inputs.txt false
-./conversion testcases_conversion/1000x1000_0_0_inputs.txt false
-./conversion testcases_conversion/4000x4000_3109_7706_inputs.txt false
+# ./conversion testcases_conversion/20x20_3_2_inputs.txt false
+# ./conversion testcases_conversion/200x200_369_445_inputs.txt false
+# ./conversion testcases_conversion/1000x1000_0_0_inputs.txt false
+# ./conversion testcases_conversion/4000x4000_3109_7706_inputs.txt false
+./conversion testcases_conversion/6000x6000_0_0_inputs-3.txt false
