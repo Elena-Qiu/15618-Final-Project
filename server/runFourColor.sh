6b90c0cb01e7ad1d18b3e2862b214301d844ae91
Qiu Yuqing <qyuqing@umich.edu>
2023-05-07 17:12:44 -0400

    rm files

--- server/runFourColor.sh
+++
@@ -1,5 +0,0 @@
-make fourcolor
-./fourcolor testcases/good_40_100_4s.txt false
-./fourcolor testcases/good_40_100_5s.txt false
-./fourcolor testcases/good_40_100_9s.txt false
-./fourcolor testcases/good_40_100_15s.txt false
